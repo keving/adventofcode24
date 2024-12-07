bdb2854582aedb27d120313204392ff32a13e2d0
Kevin Glynn <kevin.glynn@gmail.com>
2024-12-07 14:24:29 +0100

    day1.2

--- app/Main.hs
+++ app/Main.hs
@@ -20,9 +20,8 @@ parseLocIdLists = many $ do
   el2 <- many1 digit <* spaces <&> read
   return (el1, el2)
 
-
-day1 :: IO ()
-day1 = do
+day1_1 :: IO ()
+day1_1 = do
   inp <- getContents
   let (ls,rs) = unzip $ fromRight [] $ parse parseLocIdLists "(input)" inp
   print $ sum $ zipWith (\x y -> abs $ x - y) (sort ls) (sort rs)
@@ -32,6 +31,19 @@ day1_2 = do
   inp <- getContents
   let (ls,rs) = unzip $ fromRight [] $ parse parseLocIdLists "(input)" inp
   print $ foldl (\s el -> s + (el * length (filter (== el) rs))) 0 ls
-  
+
+day2_1 :: IO ()
+day2_1 = do
+  inp <- getContents
+  let reports = map (fromRight []. parse parseInts "(input)") $ lines inp
+  print $ length $ filter valid $ map listIncs reports
+  where
+    parseInts :: Parsec String () [Int]
+    parseInts = many1 $ do int <* spaces
+    listIncs (x:y:ys) =(x-y):listIncs (y:ys)
+    listIncs _ = []
+    valid xs = (all (>0) xs || all (<0) xs) && all ((<= 3) . abs) xs
+
+
 main :: IO ()
-main = day1_2
+main = day2_1
